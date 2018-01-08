81086eab2d89a06a01f61adedaeed52044db5149
Micah Vertal <micahmail9@gmail.com>
2018-01-08 16:40:26 -0500

    1.2.0 - ImageLibSTD

---
+++ Shared/ImageLib/ImageClient.cs
@@ -0,0 +1,122 @@
+﻿using ConnectLib;
+using ConnectLib.Data;
+using ConnectLib.Networking;
+using ConnectLib.Types;
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace ImageLib
+{
+    public class ImageClient : Client
+    {
+        #region Constructors
+        public ImageClient(string name, bool internalConnection, SecureString password) : base(name, internalConnection, password)
+        {
+            OnCustomCommand += Custom;
+        }
+        #endregion
+
+        #region Controls
+        public override void Connect(IPAddress remote, int port)
+        {
+            base.Connect(remote, port);
+            while (Clients.Count == 0) { }
+            Pause(true);
+            Dictionary<string, object> arguments = new Dictionary<string, object>();
+            arguments.Add("Imaging", "Start");
+            ClientInterface.Connections["Main"].Write(Password, new Command(null, ClientInterface.Information, CommandType.Custom) { Properties = arguments });
+            readCommand:
+            Command response = ClientInterface.Connections["Main"].Read<Command>(Password);
+            if (response == null || response.Properties == null)
+                goto readCommand;
+            Socket receiving = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            Socket sending = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            receiving.Connect(remote, int.Parse(response.Properties["SendingPort"].ToString()));
+            sending.Connect(remote, int.Parse(response.Properties["ReceivingPort"].ToString()));
+            ClientInterface.Connections.Add("ImageReceiver", new Connection(receiving, true, new Thread(() => { WatchForImages(); })));
+            ClientInterface.Connections.Add("ImageSender", new Connection(sending, true));
+            ClientInterface.Connections["ImageReceiver"].StartHandler();
+            Pause(false);
+            OnConnected?.BeginInvoke(result => OnConnected.EndInvoke(result), null);
+        }
+        public override async Task ConnectAsync(IPAddress remote, int port)
+        {
+            await Task.Run(() => Connect(remote, port));
+        }
+        public override void Disconnect()
+        {
+            base.Disconnect();
+            OnDisconnected?.BeginInvoke(result => OnDisconnected.EndInvoke(result), null);
+        }
+        public override async Task DisconnectAsync()
+        {
+            await Task.Run(() => Disconnect());
+        }
+        #endregion
+
+        #region Disposing
+        public override void Dispose()
+        {
+            if (Active)
+                Disconnect();
+            base.Dispose();
+        }
+        #endregion
+
+        #region Event Handlers
+        private void Custom(Command command)
+        {
+            if (command.Properties.ContainsKey("Imaging") && (string)command.Properties["Imaging"] == "Stop")
+                Disconnect();
+        }
+        private void WatchForImages()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (ClientInterface.Connections["ImageReceiver"].DataAvailable)
+                        OnImageReceived?.BeginInvoke(ClientInterface.Connections["ImageReceiver"].Read<byte[]>(Password), result => OnImageReceived.EndInvoke(result), null);
+                    else
+                        Thread.Sleep(0);
+                }
+            }
+            catch (ThreadInterruptedException) { }
+        }
+        #endregion
+
+        #region Other Methods
+        public void WriteImage(byte[] imageData)
+        {
+            ClientInterface.Connections["ImageSender"].Write(Password, imageData);
+        }
+        public async Task WriteImageAsync(byte[] imageData)
+        {
+            await Task.Run(() => WriteImage(imageData));
+        }
+        #endregion
+
+        #region Variables
+        #region Actions
+        /// <summary>
+        /// Triggered when the Client successfully connects to a remote session.
+        /// </summary>
+        public Action OnConnected { get; set; }
+        /// <summary>
+        /// Triggered when the current Client successfully disconnects from a remote session.
+        /// </summary>
+        public Action OnDisconnected { get; set; }
+        /// <summary>
+        /// Triggered when an image is received from the server
+        /// </summary>
+        public Action<byte[]> OnImageReceived { get; set; }
+        #endregion
+        #endregion
+    }
+}
---
+++ Shared/ImageLib/ImageServer.cs
@@ -0,0 +1,148 @@
+﻿using ConnectLib;
+using ConnectLib.Data;
+using ConnectLib.Networking;
+using ConnectLib.Types;
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace ImageLib
+{
+    public class ImageServer : Server
+    {
+        #region Constructors
+        public ImageServer(SecureString password) : base(password)
+        {
+            OnCustomCommand += Custom;
+        }
+        #endregion
+
+        #region Controls
+        public override void Start(int port)
+        {
+            Port = port;
+            base.Start(port);
+            OnStarted?.BeginInvoke(result => OnStarted.EndInvoke(result), null);
+        }
+        public override async Task StartAsync(int port)
+        {
+            await Task.Run(() => Start(port));
+        }
+        public override void Stop()
+        {
+            if (Clients.Count > 0)
+            {
+                Dictionary<string, object> arguments = new Dictionary<string, object>();
+                arguments.Add("Imaging", "Stop");
+                foreach (ClientObject client in Clients.Values)
+                    client.Connections["Main"].Write(Password, new Command(client.Information, Information, CommandType.Custom) { Properties = arguments });
+            }
+            base.Stop();
+            OnStopped?.BeginInvoke(result => OnStopped.EndInvoke(result), null);
+            Port = -1;
+        }
+        public override async Task StopAsync()
+        {
+            await Task.Run(() => Stop());
+        }
+        #endregion
+
+        #region Disposing
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && Active)
+                Stop();
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        #region Other Methods
+        public void WriteImage(byte[] imageData)
+        {
+            if (Clients.Count > 0)
+            {
+                foreach (ClientObject client in Clients.Values)
+                {
+                    try
+                    {
+                        if (client.Connections.ContainsKey("ImageSender"))
+                            client.Connections["ImageSender"].Write(Password, imageData);
+                    }
+                    catch { }
+                }
+            }
+        }
+        public async Task WriteImageAsync(byte[] imageData)
+        {
+            await Task.Run(() => WriteImage(imageData));
+        }
+
+        private void Custom(Command command)
+        {
+            string imageCommand = (string)command.Properties["Imaging"];
+            if (imageCommand == "Start")
+            {
+                Pause(true);
+                Dictionary<string, object> arguments = new Dictionary<string, object>();
+                arguments.Add("ReceivingPort", (Port + (6 * (Clients.Count - 1) + 1)));
+                arguments.Add("SendingPort", (Port + (6 * (Clients.Count - 1) + 2)));
+                Clients[command.Sender.ID].Connections["Main"].Write(Password, new Command(command.Sender, Information, CommandType.Custom) { Properties = arguments });
+                using (Socket receiver = new Socket(SocketType.Stream, ProtocolType.Tcp))
+                using (Socket sender = new Socket(SocketType.Stream, ProtocolType.Tcp))
+                {
+                    receiver.Bind(new IPEndPoint(Information.IP, (int)arguments["ReceivingPort"]));
+                    sender.Bind(new IPEndPoint(Information.IP, (int)arguments["SendingPort"]));
+                    receiver.Listen(1);
+                    sender.Listen(1);
+                    Clients[command.Sender.ID].Connections.Add("ImageReceiver", new Connection(receiver.Accept(), true, new Thread(() => { WatchForImages(command.Sender); })));
+                    Clients[command.Sender.ID].Connections.Add("ImageSender", new Connection(sender.Accept(), true));
+                    Clients[command.Sender.ID].Connections["ImageReceiver"].StartHandler();
+                }
+                Pause(false);
+            }
+        }
+        private void WatchForImages(ClientInformation client)
+        {
+            try
+            {
+                while (true)
+                {
+                    if (Clients[client.ID].Connections["ImageReceiver"].DataAvailable)
+                        OnImageReceived?.BeginInvoke(Clients[client.ID].Connections["ImageReceiver"].Read<byte[]>(Password), client, result => OnImageReceived.EndInvoke(result), null);
+                    else
+                        Thread.Sleep(0);
+                }
+            }
+            catch (ThreadInterruptedException) { }
+            catch (KeyNotFoundException) { }
+        }
+        #endregion
+
+        #region Variables
+        #region Actions
+        /// <summary>
+        /// Triggered when an image is received from a client
+        /// </summary>
+        public Action<byte[], ClientInformation> OnImageReceived { get; set; }
+        /// <summary>
+        /// Triggered when the Server starts successfully.
+        /// </summary>
+        public Action OnStarted { get; set; }
+        /// <summary>
+        /// Triggered when the Server stops successfully.
+        /// </summary>
+        public Action OnStopped { get; set; }
+        #endregion
+
+        /// <summary>
+        /// The port that the Server is listening on.
+        /// </summary>
+        private int Port { get; set; } = -1;
+        #endregion
+    }
+}
